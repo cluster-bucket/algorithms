4a23828301275b10e3467da90d9c819cbf13130b
Dustin Boston <dustin@dblogit.com>
2016-03-08 19:54:44 -0700

    Port to C#

---
+++ src/Algorithms/InsertionSort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Algorithms
+{
+    public class InsertionSort
+    {
+        public int[] Sort(int[] A)
+        {
+
+            for (int j = 1; j < A.Length; j++)
+            {
+                int key = A[j];
+
+                // Insert A[j] into the sorted sequence A[1..j-1]
+                int i = j - 1;
+
+                while (i >= 0 && A[i] > key)
+                {
+                    A[i + 1] = A[i];
+                    i = i - 1;
+                }
+
+                A[i + 1] = key;
+            }
+
+            return A;
+        }
+
+        public static IEnumerable<object[]> TestData
+        {
+            get
+            {
+                yield return new object[] {
+                    new int[] {  5, 2, 4, 6, 1, 3 }
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData("TestData")]
+        public void IsCorrect(int[] A)
+        {
+            Assert.Equal(A, Sort(A));
+        }
+    }
+}
---
+++ src/Algorithms/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Algorithms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Algorithms")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("72cb08f5-77ab-40e1-8396-e55a6e7799b3")]
